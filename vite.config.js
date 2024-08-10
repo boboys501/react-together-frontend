92215738ced9346ea919e37bf822f4fe88e9403b
Jie-Yu, Wang <n1056403@ntub.edu.tw>
2024-08-11 02:19:04 +0800

    init: set sever to connect with api

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://react-together-api.cophr.net',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
 
 
